3ca168b9766b9d83f8dabe95ce72e00d5bc648f0
Faaiz124 <121377490+Faaiz124@users.noreply.github.com>
2023-03-20 16:29:21 +0500

    Add files via upload
    add new assigment chapter 25 to 30

---
+++ java script/3-20-2023 ch26 to ch30/app.js
@@ -0,0 +1,82 @@
+// Q no1 Chapter 26 to 30 ......................
+// var get = prompt(" ");
+// var number = get;
+// console.log(number)
+
+// PArt B
+// var get = prompt(" ");
+// var number = Math.round(get);
+// console.log(number)
+
+// Part C
+// var get = prompt(" ");
+// var number = Math.floor(get);
+// console.log(number)
+
+
+// Part D
+// var get = prompt(" ");
+// var number = Math.ceil(get);
+// console.log(number)
+
+
+
+// Q no 2 Chapter 26 to 30 .........................
+// var get = prompt(" ");
+// var number = get;
+// console.log(number)
+
+// PArt B
+// var get = prompt(" ");
+// var number = Math.round(get);
+// console.log(number)
+
+// Part C
+// var get = prompt(" ");
+// var number = Math.floor(get);
+// console.log(number)
+
+
+// Part D
+// var get = prompt(" ");
+// var number = Math.ceil(get);
+// console.log(number)
+
+// Q no 3 Chapter 26 to 30 .........................
+// var get = prompt(" ");
+// var number = Math.abs(get);
+// console.log(number)
+
+
+// Q no 4 Chapter 26 to 30 .........................
+// var get = prompt(" ");
+// var number = Math.abs(get);
+// console.log(number)
+
+
+// Q no 5 Chapter 26 to 30 .........................
+// var random = Math.floor(Math.random() * 7);
+// document.write("random dice value: " + " " + random)
+
+
+// Q no 6 Chapter 26 to 30 .........................
+// var random = Math.floor(Math.random() * 101);
+// document.write("random number between 01 and 100: " + " " + random)
+
+
+// Q no 7 Chapter 26 to 30 .........................
+// var get = prompt(" ");
+// var number = (get);
+// document.write("The weight of the user is : " + " " + number)
+
+
+// Q no 8 Chapter 26 to 30 .........................
+// var get = prompt();
+// var number = "6"
+
+// if (get === number) {
+//     document.write("congratulation")
+// } else {
+//     document.write("Sorry wrong")
+
+// }
